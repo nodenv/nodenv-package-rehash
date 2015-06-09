08062c4d545b201f8a0e31822c0f38db4431c9ce
Jason Karns <jason@karns.name>
2015-06-08 20:08:40 -0400

    don't cd, just use full path for ln command

--- bin/install.sh
+++ bin/install.sh
@@ -8,11 +8,7 @@ NPM_HOOKS_DIR="$NODE_DIR/lib/node_modules/.hooks"
 
 mkdir -p "$NPM_HOOKS_DIR"
 
-# Remember the current directory, then change to the plugin's root.
-pushd "${BASH_SOURCE%/*}/../"
 
 # install the postinstall/postuninstall hook scripts by symlinking them
-ln -nsf "$(pwd)/libexec/nodenv-rehash.sh" "${NPM_HOOKS_DIR}/postinstall"
-ln -nsf "$(pwd)/libexec/nodenv-rehash.sh" "${NPM_HOOKS_DIR}/postuninstall"
-
-popd
+ln -nsf "${BASH_SOURCE%/*}/../libexec/nodenv-rehash.sh" "${NPM_HOOKS_DIR}/postinstall"
+ln -nsf "${BASH_SOURCE%/*}/../libexec/nodenv-rehash.sh" "${NPM_HOOKS_DIR}/postuninstall"
