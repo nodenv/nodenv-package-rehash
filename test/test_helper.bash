aef128a1ffdd3f3371c9daa45ac2863752c2c518
Jason Karns <jason@karns.name>
2015-06-12 16:53:13 -0400

    corrected pathing

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,3 +1,5 @@
+load helpers/assertions/all
+
 export TMP="$BATS_TEST_DIRNAME/tmp"
 
 NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
@@ -16,14 +18,6 @@ teardown() {
   rm -rf "$TMP"
 }
 
-flunk() {
-  { if [ "$#" -eq 0 ]; then cat -
-    else echo "$@"
-    fi
-  } | sed "s:${NODENV_TEST_DIR}:TEST_DIR:g" >&2
-  return 1
-}
-
 # Creates fake version directories
 create_versions() {
   for v in $*
@@ -40,38 +34,6 @@ assert_package_hooks() {
   assert_line FOO
 }
 
-assert_success() {
-  if [ "$status" -ne 0 ]; then
-    flunk "command failed with exit status $status"
-  elif [ "$#" -gt 0 ]; then
-    assert_output "$1"
-  fi
-}
-
-assert_failure() {
-  if [ "$status" -eq 0 ]; then
-    flunk "expected failed exit status"
-  elif [ "$#" -gt 0 ]; then
-    assert_output "$1"
-  fi
-}
-
-assert_equal() {
-  if [ "$1" != "$2" ]; then
-    { echo "expected: $1"
-      echo "actual:   $2"
-    } | flunk
-  fi
-}
-
-assert_output() {
-  local expected
-  if [ $# -eq 0 ]; then expected="$(cat -)"
-  else expected="$1"
-  fi
-  assert_equal "$expected" "$output"
-}
-
 assert_line() {
   if [ "$1" -ge 0 ] 2>/dev/null; then
     assert_equal "$2" "${lines[$1]}"
@@ -84,40 +46,6 @@ assert_line() {
     expected line '$1' in:
 ${output}
 MSG
-  fi
-}
-
-assert_line_starts_with() {
-  if [ "$1" -ge 0 ] 2>/dev/null; then
-    assert_equal "$2" "${lines[$1]}"
-  else
-    local line
-    for line in "${lines[@]}"; do
-      if [ -n "${line#${1}}" ]; then return 0; fi
-    done
-    flunk "expected line \`$1'"
-  fi
-}
-
-refute_line() {
-  if [ "$1" -ge 0 ] 2>/dev/null; then
-    local num_lines="${#lines[@]}"
-    if [ "$1" -lt "$num_lines" ]; then
-      flunk "output has $num_lines lines"
-    fi
-  else
-    local line
-    for line in "${lines[@]}"; do
-      if [ "$line" = "$1" ]; then
-        flunk "expected to not find line \`$line'"
-      fi
-    done
-  fi
-}
-
-assert() {
-  if ! "$@"; then
-    flunk "failed: $@"
   fi
 }
 
