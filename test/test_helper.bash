7aa899a81e07cb43cea1df7fc2ee5f5cda6d929b
Jason Karns <jason@karns.name>
2015-11-10 09:39:05 -0500

    don't need $TMP

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,8 +1,6 @@
 load ../node_modules/bats-assert/all
 load ../node_modules/bats-mock/stub
 
-export TMP="$BATS_TEST_DIRNAME/tmp"
-
 NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
 mkdir -p "${NODENV_TEST_DIR}"
 
@@ -11,13 +9,11 @@ export NODENV_ROOT="${NODENV_TEST_DIR}"
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 PATH="$BATS_TEST_DIRNAME/helpers/bin:$PATH"
 PATH="$BATS_TEST_DIRNAME/../bin:$PATH"
-PATH="$TMP/bin:$PATH"
 PATH="$BATS_MOCK_BINDIR:$PATH"
 export PATH
 
 teardown() {
   rm -rf "$NODENV_TEST_DIR"
-  rm -rf "$TMP"
 }
 
 # Creates fake version directories
