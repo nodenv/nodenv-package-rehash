7efe1894418cd5bf85cb2af4944d2799abe34535
Jason Karns <jason@karns.name>
2015-11-09 19:13:14 -0500

    use bats-mock for the stubs

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,4 +1,5 @@
 load ../node_modules/bats-assert/all
+load ../node_modules/bats-mock/stub
 
 export TMP="$BATS_TEST_DIRNAME/tmp"
 
@@ -11,6 +12,7 @@ PATH=/usr/bin:/bin:/usr/sbin:/sbin
 PATH="$BATS_TEST_DIRNAME/helpers/bin:$PATH"
 PATH="$BATS_TEST_DIRNAME/../bin:$PATH"
 PATH="$TMP/bin:$PATH"
+PATH="$BATS_MOCK_BINDIR:$PATH"
 export PATH
 
 teardown() {
@@ -40,35 +42,4 @@ refute_package_hooks() {
   version="$1"
   test ! -f "$NODENV_ROOT/versions/$version/lib/node_modules/.hooks/postinstall"
   test ! -f "$NODENV_ROOT/versions/$version/lib/node_modules/.hooks/postuninstall"
-}
-
-stub() {
-  local program="$1"
-  local prefix="$(echo "$program" | tr a-z- A-Z_)"
-  shift
-
-  export "${prefix}_STUB_PLAN"="${TMP}/${program}-stub-plan"
-  export "${prefix}_STUB_RUN"="${TMP}/${program}-stub-run"
-  export "${prefix}_STUB_END"=
-
-  mkdir -p "${TMP}/bin"
-  ln -sf "${BATS_TEST_DIRNAME}/helpers/stub" "${TMP}/bin/${program}"
-
-  touch "${TMP}/${program}-stub-plan"
-  for arg in "$@"; do printf "%s\n" "$arg" >> "${TMP}/${program}-stub-plan"; done
-}
-
-unstub() {
-  local program="$1"
-  local prefix="$(echo "$program" | tr a-z- A-Z_)"
-  local path="${TMP}/bin/${program}"
-
-  export "${prefix}_STUB_END"=1
-
-  local STATUS=0
-  "$path" || STATUS="$?"
-
-  rm -f "$path"
-  rm -f "${TMP}/${program}-stub-plan" "${TMP}/${program}-stub-run"
-  return "$STATUS"
 }
