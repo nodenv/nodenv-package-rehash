e67e08c639e471a5d8c0157c15a2496b5acf8e53
Jason Karns <jason@karns.name>
2015-11-17 10:26:23 -0500

    exit gracefully from failed installation

--- etc/nodenv.d/install/install-pkg-hooks.bash
+++ etc/nodenv.d/install/install-pkg-hooks.bash
@@ -1,6 +1,8 @@
 after_install install_hook_scripts
 
 install_hook_scripts() {
+  if [ "$STATUS" != 0 ]; then return; fi
+
   nodenv-package-hooks install "$VERSION_NAME"
   echo "Installed postinstall/postuninstall package hooks for $VERSION_NAME"
 }
